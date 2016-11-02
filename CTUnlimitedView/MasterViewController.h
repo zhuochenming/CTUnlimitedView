765feead1144a01646cb981730955402ab06f0cb
酌晨茗 <294503386@qq.com>
2016-11-02 13:18:58 +0800

    Initial Commit

---
+++ CTUnlimitedView/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  CTUnlimitedView
+//
+//  Created by boleketang on 2016/11/2.
+//  Copyright © 2016年 Zhuochenming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
